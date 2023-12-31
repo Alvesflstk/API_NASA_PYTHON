1b1bc52960b02543e014e426dcc7acdff27f96ea
Gabriel Alves <pcdogabriel23@gmail.com>
2023-12-31 10:45:54 -0300

    remasterize apllication

--- API.py
+++ API.py
@@ -1,9 +1,13 @@
-from flask import Flask ,jsonify ,render_template
-
-
+from flask import Flask,render_template,jsonify
 api = Flask(__name__)
 
+def dados_capacete(arquivo):
+    with open(f'{arquivo}','r') as metadados:
+        return metadados.read()
 
+def dados_traje(arquivo):
+    with open(f'{arquivo}','r') as metadados:
+        return metadados.read()
 
 @api.route('/')
 def home():
@@ -12,16 +16,11 @@ def home():
 
 @api.route('/confidencial')
 def response_estaplanetar():
-    with open('x.txt','r') as arquivoX:
-        confidencial = arquivoX.read()
+    dr_man_capacete = dados_capacete('astronautas/capacete/dr_man.txt')
+    dr_man_traje = dados_traje('astronautas/roupa/dr_man.txt')
+    dra_brand_capacete = dados_capacete('astronautas/capacete/dra_brand.txt')
+    dra_brand_traje = dados_traje('astronautas/roupa/dra_brand.txt')
 
-    with open('loc.txt','r') as arquivoloc:
-        loc = arquivoloc.read()
-    
-    with open('exploracao.txt','r') as exp:
-        expExpedicao = exp.read()
-
-    dados ={'confidencial':confidencial,'localizacao':loc,'expedicao':expExpedicao}
-
+    dados = {'dr.man_capacete':dr_man_capacete,'dra.brand_capacete':dra_brand_capacete,'dr.man_traje':dr_man_traje,'dra.man_traje':dra_brand_traje}
     return jsonify(dados)
 api.run(port=5000,host='localhost',debug=True)
